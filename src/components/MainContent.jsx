c4779d556d0de6e99fce97b24441259fd8924f00
Chuong Tran <39957959+trananhchuong@users.noreply.github.com>
2024-08-04 13:40:00 +0700

    Update MainContent.jsx

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -11,7 +11,7 @@ function MainContent() {
 
   return (
     <main>
-      <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
+      <button onClick={toggleHelp} className="button">{helpVisible ? 'Hide' : 'Show'} Help</button>
       {helpVisible && <HelpArea />}
     </main>
   );
