ae827e394e824f509b447c371d8ef2bac11226da
Chuong Tran <39957959+trananhchuong@users.noreply.github.com>
2024-08-04 12:39:49 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <header>
         <div id="logo-img">
-          <img src={logo} />
+          <img src={logo} allt="logo" />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
       </header>
